a66bb1bdaa768c8927a8c53717751b2ec8b1dcd2
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2023-09-11 15:34:02 +0200

    made the start site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    colors: {
+      "yellow": "#e7eca3",
+      "black": "#0e1111",
+      "white": "#FFFDFA",
+      "purple": "#512bd4"
+    },
     extend: {},
   },
   plugins: [],
